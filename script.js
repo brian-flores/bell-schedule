755df2e69001868db0f866d79b8e5fdcd8bab15d
Brian Flores <bflores29@yisd.net>
2025-08-12 22:39:55 -0600

    Updates to schedules

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const scheduleFirst = [
+  { period: "Zero Period (0)", start: "7:35 AM", end: "8:15 AM" },
   { period: "Warning Bell", start: "8:25 AM", end: "8:25 AM" },
   { period: "Period 1", start: "8:30 AM", end: "9:15 AM" },
   { period: "Period 2", start: "9:20 AM", end: "10:05 AM" },
   { period: "Period 3", start: "10:10 AM", end: "10:55 AM" },
   { period: "Period 4", start: "11:00 AM", end: "11:45 AM" },
-  { period: "Lunch", start: "11:45 AM", end: "12:26 PM" },
-  { period: "Advisory", start: "12:26 PM", end: "12:56 PM" },
-  { period: "Period 5", start: "1:00 PM", end: "1:45 PM" },
-  { period: "Period 6", start: "1:50 PM", end: "2:35 PM" },
-  { period: "Period 7", start: "2:40 PM", end: "3:25 PM" },
-  { period: "Period 8", start: "3:30 PM", end: "4:15 PM" },
-  { period: "Zero Period (0)", start: "7:35 AM", end: "8:15 AM" }
+  { period: "Period 5 (Lunch & Advisory)", start: "11:45 AM", end: "12:56 PM" },
+  { period: "Period 6", start: "1:00 PM", end: "1:45 PM" },
+  { period: "Period 7", start: "1:50 PM", end: "2:35 PM" },
+  { period: "Period 8", start: "2:40 PM", end: "3:25 PM" },
+  { period: "Period 9", start: "3:30 PM", end: "4:15 PM" },
+  { period: "Period 10", start: "4:20 PM", end: "5:05 PM" }
 ];
 
 const scheduleSecond = [
+  { period: "Zero Period (0)", start: "7:35 AM", end: "8:15 AM" },
   { period: "Warning Bell", start: "8:25 AM", end: "8:25 AM" },
   { period: "Period 1", start: "8:30 AM", end: "9:15 AM" },
   { period: "Period 2", start: "9:20 AM", end: "10:05 AM" },
   { period: "Period 3", start: "10:10 AM", end: "10:55 AM" },
   { period: "Period 4", start: "11:00 AM", end: "11:45 AM" },
-  { period: "Advisory", start: "11:49 AM", end: "12:19 PM" },
-  { period: "Lunch", start: "12:19 PM", end: "1:00 PM" },
-  { period: "Period 5", start: "1:00 PM", end: "1:45 PM" },
-  { period: "Period 6", start: "1:50 PM", end: "2:35 PM" },
-  { period: "Period 7", start: "2:40 PM", end: "3:25 PM" },
-  { period: "Period 8", start: "3:30 PM", end: "4:15 PM" },
-  { period: "Zero Period (0)", start: "7:35 AM", end: "8:15 AM" }
+  { period: "Period 5", start: "11:49 AM", end: "12:19 PM" },
+  { period: "Period 6 (Advisory & Lunch)", start: "12:19 PM", end: "1:00 PM" },
+  { period: "Period 7", start: "1:00 PM", end: "1:45 PM" },
+  { period: "Period 8", start: "1:50 PM", end: "2:35 PM" },
+  { period: "Period 9", start: "2:40 PM", end: "3:25 PM" },
+  { period: "Period 10", start: "3:30 PM", end: "4:15 PM" }
 ];
 
 function updateCurrentTime() {
